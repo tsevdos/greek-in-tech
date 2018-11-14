3f1e69abddc086a9a37fcd9f4f53233b8a87f5f2
John Tsevdos <tsevdosjohn@gmail.com>
2018-11-14 14:08:04 +0200

    Add Jest test runner - remove Mocha, chai, etc. (#9)

--- test/index.test.js
+++ test/index.test.js
@@ -1,82 +1,54 @@
-import {expect} from 'chai';
 import * as m from '../src';
 
 describe('Greek in Tech', () => {
   describe('all', () => {
     it('must be an array with length more than zero', () => {
-      expect(m.all).to.be.an('array');
-      expect(m.all.length).to.be.above(0);
+      expect(Array.isArray(m.all)).toBe(true);
+      expect(m.all.length).toBeGreaterThan(0);
     });
 
     it('must includes the "Daemon" entry', () => {
-      const entryDescription = [
-        'The term coined by programmers of MIT\'s Project MAC, was inspired by the physicist',
-        'James Clerk Maxwell\'s demon. It originated as an imaginary being from a thought',
-        'experiment that constantly works in the background sorting molecules.',
-        'In Greek mythology, a daemon is a supernatural being working in the background,',
-        'with no particular bias towards good or evil. The daemon concept was subsequently',
-        'adopted by Unix systems, however, BSD and some of its derivatives have used a',
-        'Christian interpretation of the mythological deamon as their mascot rather than',
-        'a Greek daemon.'
-      ];
-      const entry = {
-        id: 2,
-        title: 'Daemon',
-        description: entryDescription.join(' '),
-        categories: [
-          'OS',
-          'unix',
-          'linux',
-          'open source'
-        ],
-        references: [
-          {
-            name: 'Wikipedia',
-            source: 'https://en.wikipedia.org/wiki/Daemon_(computing)#Etymology'
-          }
-        ]
-      };
-
-      expect(m.all).to.deep.include(entry);
+      const entry = m.all[1];
+      expect(m.all).toContain(entry);
     });
   });
 
   describe('random', () => {
     it('must return a random entry from the list', () => {
       const entry = m.random();
-      expect(m.all).to.include(entry);
+      expect(m.all).toContain(entry);
     });
   });
 
   describe('getEntry', () => {
     it('the entry must be an object with correct properties', () => {
       const entry = m.getEntry(2);
-      expect(entry).to.be.an('object');
-      expect(entry).to.have.property('id');
-      expect(entry).to.have.property('title');
-      expect(entry).to.have.property('description');
-      expect(entry).to.have.property('categories');
-      expect(entry).to.have.property('references');
-      expect(entry.references).to.be.an('array');
+      expect(typeof entry).toBe('object');
+      expect(entry).toHaveProperty('id');
+      expect(entry).toHaveProperty('title');
+      expect(entry).toHaveProperty('description');
+      expect(entry).toHaveProperty('categories');
+      expect(entry).toHaveProperty('references');
+      expect(Array.isArray(entry.references)).toBe(true);
     });
 
     it('must return a specific entry', () => {
       const entry = m.getEntry(1);
-      expect(entry.id).to.eql(1);
-      expect(entry.title).to.eql('Cron (job scheduler)');
+      expect(entry.id).toBe(1);
+      expect(entry.title).toBe('Cron (job scheduler)');
       const description = [
         'It comes from the Greek word for time, chronos (χρόνος).',
         'Chronos is the personification of time in early Greek mythology and literature.',
         'He later appears in the Renaissance as Father Time.'
       ];
-      expect(entry.description).to.eql(description.join(' '));
-      expect(entry.categories).to.eql(['OS', 'unix', 'linux', 'open source']);
-      expect(entry.references[0].name).to.eql('Wikipedia');
-      expect(entry.references[0].source).to.eql('https://en.wikipedia.org/wiki/Cron');
+      expect(entry.description).toBe(description.join(' '));
+      expect(entry.categories).toEqual(['OS', 'unix', 'linux', 'open source']);
+      expect(entry.references[0].name).toBe('Wikipedia');
+      expect(entry.references[0].source).toBe('https://en.wikipedia.org/wiki/Cron');
     });
 
     it('must throw an error if entry doesnt exists', () => {
-      expect(m.getEntry.bind(m, 10001)).to.throw(Error, /Invalid ID/);
+      expect(() => m.getEntry(10001)).toThrow('Invalid ID');
     });
   });
 });
